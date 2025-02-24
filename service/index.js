ff17aed93ffea94612d170afaf793b87f9d39a15
RyanVictorDev <vryan8294@gmail.com>
2025-02-24 18:25:35 -0300

    create: creating check and delete functions

--- service/index.js
+++ service/index.js
@@ -1,11 +1,16 @@
-axios.get('https://to-do-list-nodejs-43as.onrender.com/note')
-  .then(response => {
-    console.log('Notas:', response.data);
-    
-    const noteContainer = document.getElementById('note');
-    noteContainer.innerHTML = '';
+window.onload = () => {
+    fethNotes()
+}
 
-    response.data.forEach(element => {
+function fethNotes() {
+    axios.get('https://to-do-list-nodejs-43as.onrender.com/note')
+    .then(response => {
+        console.log('Notas:', response.data);
+    
+        const noteContainer = document.getElementById('note');
+        noteContainer.innerHTML = '';
+
+        response.data.forEach(element => {
         const noteDiv = document.createElement('div');
         noteDiv.classList.add(element.finished ? 'noteItem' : 'noteItemPending');
 
@@ -22,32 +27,62 @@ axios.get('https://to-do-list-nodejs-43as.onrender.com/note')
         arrowIcon.classList.add('material-icons');
         arrowIcon.textContent = 'keyboard_arrow_down';
 
+        const description = document.createElement('p');
+        description.textContent = element.description;
+        description.classList.add('description', 'hidden');
+
+        const actionsDiv = document.createElement('div');
+        actionsDiv.classList.add('actions', 'hidden');
+
+        const deleteButton = document.createElement('button');
+        deleteButton.classList.add('deleteButton');
+        deleteButton.onclick = () => deleteNote(element.id);
+
+        const deleteIcon = document.createElement('i');
+        deleteIcon.classList.add('material-icons');
+        deleteIcon.textContent = 'delete'
+
+        deleteButton.appendChild(deleteIcon);
+        actionsDiv.appendChild(deleteButton);
+
+        const finishButton = document.createElement('button');
+        finishButton.classList.add('finishButton');
+        finishButton.onclick = () => deleteNote(element.id);
+
+        const checkIcon = document.createElement('i');
+        checkIcon.classList.add('material-icons');
+        checkIcon.textContent = 'check';
+
+        finishButton.appendChild(checkIcon);
+        actionsDiv.appendChild(finishButton);
+        finishButton.onclick = () => checkNote(element.id);
+
         arrowBtn.appendChild(arrowIcon);
         titleDiv.appendChild(title);
         titleDiv.appendChild(arrowBtn);
         noteDiv.appendChild(titleDiv);
 
-        const description = document.createElement('p');
-        description.textContent = element.description;
-        description.classList.add('description', 'hidden');
-
         noteDiv.appendChild(description);
+        noteDiv.appendChild(actionsDiv);
         noteContainer.appendChild(noteDiv);
 
         titleDiv.addEventListener('click', () => {
-            showMore(description, arrowIcon);
+            showMore(description, actionsDiv, arrowIcon);
         });
-    });
-  })
-  .catch(error => {
-    console.error('Erro ao buscar notas:', error);
-  });
-
-function showMore(description, arrowIcon) {
-    description.classList.toggle('hidden');
-    arrowIcon.textContent = description.classList.contains('hidden') ? 'keyboard_arrow_down' : 'keyboard_arrow_up';
+        });
+    })
+    .catch(error => {
+        console.error('Erro ao buscar notas:', error);
+    })    
 }
 
+
+function showMore(description, actionsDiv, arrowIcon) {
+    description.classList.toggle('hidden');
+    actionsDiv.classList.toggle('hidden');
+    arrowIcon.textContent = description.classList.contains('hidden') ? 'keyboard_arrow_down' : 'keyboard_arrow_up';
+}
+ 
 const noteCreate = {
     title: '',
     description: ''
@@ -71,3 +106,27 @@ document.getElementById('noteForm').addEventListener('submit', function(event) {
         console.log(e)
     })
 });
+
+function deleteNote(id) {
+    axios.delete('https://to-do-list-nodejs-43as.onrender.com/note/' + id)
+    .then( () => {
+        console.log('Sucesso'),
+        fethNotes()
+    }) .catch (e => {
+        console.log('IH RAPAZ', e)
+    })
+}
+
+function checkNote(id) {
+    axios.put('https://to-do-list-nodejs-43as.onrender.com/note/finish/' + id)
+    .then(() => {
+        console.log('Sucesso!');
+        fethNotes(); 
+        setTimeout(() => {
+            location.reload(); 
+        }, 500);
+    })
+    .catch(e => {
+        console.log('Deu ruim', e);
+    });
+}
